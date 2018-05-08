b6e65e9d993a9bc34dd48c8eb74441f6ddc7702a
agustin <agustin@treexor.com>
2018-05-08 15:40:35 +0100

    Config improvements

--- src/main/java/com/txrlabs/microservice/ecommerce/backgroundProcessFeedback/EcBackgroundProcessFeedbackApplication.java
+++ src/main/java/com/txrlabs/microservice/ecommerce/backgroundProcessFeedback/EcBackgroundProcessFeedbackApplication.java
@@ -3,6 +3,7 @@ package com.txrlabs.microservice.ecommerce.backgroundProcessFeedback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableCircuitBreaker
 @SpringBootApplication
 @EnableScheduling
+@RefreshScope
 public class EcBackgroundProcessFeedbackApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcBackgroundProcessFeedbackApplication.class, args);
