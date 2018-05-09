6d5bcde6bc28b5c7bdadde4bd43b0fc04b629682
agustin <agustin@treexor.com>
2018-05-09 12:44:52 +0100

    Fix Application config

--- src/main/java/com/txrlabs/microservice/ecommerce/backgroundProcessFeedback/EcBackgroundProcessFeedbackApplication.java
+++ src/main/java/com/txrlabs/microservice/ecommerce/backgroundProcessFeedback/EcBackgroundProcessFeedbackApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCircuitBreaker
 @SpringBootApplication
-@EnableScheduling
 @RefreshScope
 public class EcBackgroundProcessFeedbackApplication {
     public static void main(String[] args) {
